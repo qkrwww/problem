1b979637a27df8272f368410ced9bfa84e7468e5
qkrwww <109325229+qkrwww@users.noreply.github.com>
2022-08-26 09:06:20 +0900

    [Silver IV] Title: 큐, Time: 84 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.py
@@ -0,0 +1,30 @@
+import sys
+input=sys.stdin.readline
+a = int(input())
+s=[]
+for i in range(a):
+    b=list(map(str, input().split()))
+    if b[0]=='push':
+        s.append(b[1])
+    elif b[0]=='size':
+        print(len(s))
+    elif b[0]=='empty':
+        if len(s)==0:
+            print(1)
+        else:
+            print(0)
+    elif b[0]=='pop':
+        if len(s)==0:
+            print(-1)
+        else:
+            print(int(s.pop(0)))
+    elif b[0]=='front':
+        if len(s)==0:
+            print(-1)
+        else:
+            print(int(s[0]))
+    elif b[0]=='back':
+        if len(s)==0:
+            print(-1)
+        else:
+            print(s[-1])
