2b1b377eb515e5c2272c404612519d9ed43819d0
qkrwww <109325229+qkrwww@users.noreply.github.com>
2022-09-05 16:04:38 +0900

    [Silver III] Title: 게임, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/1072. 게임/게임.py
@@ -0,0 +1,17 @@
+N , M = map(int,input().rstrip().split())
+Z = (M *100)//N
+ans = 0
+if Z >= 99:
+    print(-1)
+else:
+    ans = 0
+    start =1
+    end = 1000000000
+    while start <= end:
+        mid = (start+end)//2
+        if (M+mid)*100 // (N+mid) > Z:
+            ans = mid
+            end = mid -1
+        else:
+            start = mid+1
+    print(ans)
